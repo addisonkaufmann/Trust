408b6310e1e31b0176572407f13cd1d73b15b643
addisonkaufmann <addisonkaufmann@email.arizona.edu>
2016-06-20 14:36:07 +0200

    jquery autoscroll fix to hash problem

--- www/js/app.js
+++ www/js/app.js
@@ -114,7 +114,7 @@ app.controller('dashCtrl', function($scope, $rootScope, $stateParams, $state){
     $rootScope.$on('$stateChangeSuccess', 
         function(event, toState, toParams, fromState, fromParams){
             $state.current = toState;
-            console.log($state.current);
+            // console.log($state.current);
         });
 
     $scope.iconhome = 'img/icons/';
@@ -271,10 +271,15 @@ app.controller('dashCtrl', function($scope, $rootScope, $stateParams, $state){
 });
 
 
-app.controller('socialCtrl', function($scope, $location, anchorSmoothScroll){
+app.controller('socialCtrl', function($scope, $location, $window, $rootScope, $anchorScroll){
     $scope.iconhome = 'img/icons/';
     $scope.expand = false;
 
+    angular.element($window).bind('resize', function () {
+    	console.log($window.innerHeight);	
+	});
+
+	
 
     $scope.showExpand = function(){
         $scope.expand = true;
@@ -286,15 +291,16 @@ app.controller('socialCtrl', function($scope, $location, anchorSmoothScroll){
         console.log('expand is now ' + $scope.expand);
     };
 
-    $scope.gotoElement = function (eID){
-
-      // set the location.hash to the id of
-      // the element you wish to scroll to.
-      $location.hash('bottom');
-      // call $anchorScroll()
-      anchorSmoothScroll.scrollTo(eID);
-      
+    $scope.scrollToBottom = function(){
+		console.log($window.innerHeight);
+	    $("html,body").animate({ scrollTop: $window.innerHeight}, "slow");
     };
+
+    $scope.scrollToTop = function(){
+	    $("html,body").animate({ scrollTop: 0}, "slow");
+    };
+
+
 
     $scope.social = [
         {
@@ -455,61 +461,61 @@ app.directive('seventiles', function(){
     };
 });
 
-app.service('anchorSmoothScroll', function(){
+// app.service('anchorSmoothScroll', function(){
     
-    this.scrollTo = function(eID) {
+//     this.scrollTo = function(eID) {
 
-        // This scrolling function 
-        // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
+//         // This scrolling function 
+//         // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
         
-        var startY = currentYPosition();
-        var stopY = elmYPosition(eID);
-        // console.log('start: ' + startY + "; stop: " + stopY);
+//         var startY = currentYPosition();
+//         var stopY = elmYPosition(eID);
+//         // console.log('start: ' + startY + "; stop: " + stopY);
     
-        var distance = stopY > startY ? stopY - startY : startY - stopY;
-        if (distance < 100) {
-            scrollTo(0, stopY); return;
-        }
-        var speed = Math.round(distance / 100);
-        if (speed >= 20) speed = 20;
-        var step = Math.round(distance / 25);
-        var leapY = stopY > startY ? startY + step : startY - step;
-        var timer = 0;
-        if (stopY > startY) {
-            for ( var i=startY; i<stopY; i+=step ) {
-                setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
-            } return;
-        }
+//         var distance = stopY > startY ? stopY - startY : startY - stopY;
+//         if (distance < 100) {
+//             scrollTo(0, stopY); return;
+//         }
+//         var speed = Math.round(distance / 100);
+//         if (speed >= 20) speed = 20;
+//         var step = Math.round(distance / 25);
+//         var leapY = stopY > startY ? startY + step : startY - step;
+//         var timer = 0;
+//         if (stopY > startY) {
+//             for ( var i=startY; i<stopY; i+=step ) {
+//                 setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+//                 leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+//             } return;
+//         }
     
-        for (  var i=startY; i>stopY; i-=step ) {
-            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
-        }
+//         for (  var i=startY; i>stopY; i-=step ) {
+//             setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+//             leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+//         }
         
-        function currentYPosition() {
-            // Firefox, Chrome, Opera, Safari
-            if (self.pageYOffset) return self.pageYOffset;
-            // Internet Explorer 6 - standards mode
-            if (document.documentElement && document.documentElement.scrollTop)
-                return document.documentElement.scrollTop;
-            // Internet Explorer 6, 7 and 8
-            if (document.body.scrollTop) return document.body.scrollTop;
-            return 0;
-        }
+//         function currentYPosition() {
+//             // Firefox, Chrome, Opera, Safari
+//             if (self.pageYOffset) return self.pageYOffset;
+//             // Internet Explorer 6 - standards mode
+//             if (document.documentElement && document.documentElement.scrollTop)
+//                 return document.documentElement.scrollTop;
+//             // Internet Explorer 6, 7 and 8
+//             if (document.body.scrollTop) return document.body.scrollTop;
+//             return 0;
+//         }
         
-        function elmYPosition(eID) {
-            var elm = document.getElementById(eID);
-            var y = elm.offsetTop;
-            var node = elm;
-            while (node.offsetParent && node.offsetParent != document.body) {
-                node = node.offsetParent;
-                y += node.offsetTop;
-            } return y;
-        }
+//         function elmYPosition(eID) {
+//             var elm = document.getElementById(eID);
+//             var y = elm.offsetTop;
+//             var node = elm;
+//             while (node.offsetParent && node.offsetParent != document.body) {
+//                 node = node.offsetParent;
+//                 y += node.offsetTop;
+//             } return y;
+//         }
 
-    };
+//     };
     
-});
+// });
 
 
