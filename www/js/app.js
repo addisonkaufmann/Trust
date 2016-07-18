ae2f0f02e95805f8794eb7a50714ad1ecd0e1e6e
addisonkaufmann <addisonkaufmann@email.arizona.edu>
2016-07-18 17:34:53 +0200

    basic translation on card.html

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,28 @@
-var app = angular.module('app', ['ui.router', 'ngSanitize', 'matchMedia', 'angular-loading-bar','chart.js']);
+var app = angular.module('app', ['ui.router', 'ngSanitize', 'matchMedia', 'angular-loading-bar','chart.js', 'pascalprecht.translate']);
 
 
 // $(window).load(function() {
 //     $('body').addClass('loaded');
 // });
 
+
+app.config(function($translateProvider){
+	var url = 'http://localhost:8080/trust/api/language/13/';
+
+	$translateProvider.useStaticFilesLoader({
+		prefix : url,
+		suffix : ''
+	});
+
+	// $translateProvider.useUrlLoader(url);
+	$translateProvider.preferredLanguage('en');
+
+
+	// traslate, default language
+	// $translateProvider.preferredLanguage('it');
+
+	// traslate, sanitize strategy
+	$translateProvider.useSanitizeValueStrategy('sanitize');
+
+});
 
